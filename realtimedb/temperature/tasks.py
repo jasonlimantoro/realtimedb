f2eb4afc82bb35ac969b2f28de4f22f6f23c5094
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-05-14 21:28:19 +0800

    FEAT: add task to update temperature every 10 seconds

---
+++ realtimedb/temperature/tasks.py
@@ -0,0 +1,28 @@
+from celery.task import periodic_task
+from celery.utils.log import get_task_logger
+from datetime import timedelta
+from .models import Temperature
+from random import uniform
+
+logger = get_task_logger(__name__)
+
+
+@periodic_task(
+    run_every=(timedelta(seconds=10)),
+    name='update-temperature',
+    ignore_result=True
+)
+def update_temperature():
+    """
+    Update temperature in db by using randomized values
+    :return: void
+    """
+    updated = []
+    for temperature in Temperature.objects.all():
+        diff = uniform(-1, 1)
+        logger.info(diff)
+        temperature.value = round(temperature.value + diff, 2)
+        temperature.save()
+        updated.append(temperature)
+
+    logger.info(f"Updated temperature: {updated}")
