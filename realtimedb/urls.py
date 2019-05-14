dd8e5d24e63e927f4cd21d37f9db1b1ef9a8f0c4
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-05-14 17:21:12 +0800

    FEAT(core): add core app
    This core app provides an example how to work with Celery

--- realtimedb/urls.py
+++ realtimedb/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from realtimedb.core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('core/generate', views.GenerateRandomUserView.as_view(), name='generate'),
+    path('core/list', views.UsersListView.as_view(), name='users_list'),
 ]
